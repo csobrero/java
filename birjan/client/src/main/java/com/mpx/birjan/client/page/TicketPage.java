a57c6ca2b5887635569ae99f530ca7deb73493d9
csobrero <csobrero@gmail.com>
2013-03-22 17:40:44 -0300

    ticket page

--- birjan/client/src/main/java/com/mpx/birjan/client/page/TicketPage.java
+++ birjan/client/src/main/java/com/mpx/birjan/client/page/TicketPage.java
@@ -3,6 +3,7 @@ package com.mpx.birjan.client.page;
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.EventQueue;
+import java.util.Vector;
 import java.util.regex.Pattern;
 
 import javax.swing.Box;
@@ -25,6 +26,11 @@ import javax.swing.table.DefaultTableModel;
 import org.apache.commons.lang.StringUtils;
 import java.awt.Font;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JRadioButton;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
 
 public class TicketPage extends JFrame {
 
@@ -55,7 +61,7 @@ public class TicketPage extends JFrame {
 	@SuppressWarnings({"serial", "unchecked", "rawtypes" })
 	public TicketPage() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 800, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -85,8 +91,51 @@ public class TicketPage extends JFrame {
 		Box horizontalBox = Box.createHorizontalBox();
 		verticalBox.add(horizontalBox);
 		
-		Component horizontalStrut = Box.createHorizontalStrut(40);
-		horizontalBox.add(horizontalStrut);
+		Box verticalBox_1 = Box.createVerticalBox();
+		horizontalBox.add(verticalBox_1);
+		
+		Component verticalStrut_3 = Box.createVerticalStrut(20);
+		verticalBox_1.add(verticalStrut_3);
+		
+		Box horizontalBox_2 = Box.createHorizontalBox();
+		verticalBox_1.add(horizontalBox_2);
+		
+		Component horizontalStrut_3 = Box.createHorizontalStrut(20);
+		horizontalBox_2.add(horizontalStrut_3);
+		
+		JPanel panel = new JPanel();
+		horizontalBox_2.add(panel);
+		
+		Box verticalBox_2 = Box.createVerticalBox();
+		panel.add(verticalBox_2);
+		
+		JLabel lblLoteria = new JLabel("Lot");
+		lblLoteria.setAlignmentX(Component.CENTER_ALIGNMENT);
+		lblLoteria.setFont(new Font("Tahoma", Font.BOLD, 18));
+		verticalBox_2.add(lblLoteria);
+		
+		Component verticalStrut_4 = Box.createVerticalStrut(10);
+		verticalBox_2.add(verticalStrut_4);
+		
+		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"NACIONAL", "PROVINCIA"}));
+		verticalBox_2.add(comboBox_1);
+		
+		Component verticalStrut_5 = Box.createVerticalStrut(10);
+		verticalBox_2.add(verticalStrut_5);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"PRIMERA", "MATUTINA", "VESPERTINA", "NOCTURNA"}));
+		verticalBox_2.add(comboBox);
+		
+		Component horizontalStrut = Box.createHorizontalStrut(120);
+		verticalBox_2.add(horizontalStrut);
+		
+		Component horizontalStrut_4 = Box.createHorizontalStrut(20);
+		horizontalBox_2.add(horizontalStrut_4);
+		
+		Component verticalStrut_2 = Box.createVerticalStrut(20);
+		verticalBox_1.add(verticalStrut_2);
 		
 		JScrollPane mainPanel = new JScrollPane();
 		mainPanel.setViewportBorder(null);
@@ -94,31 +143,54 @@ public class TicketPage extends JFrame {
 		horizontalBox.add(mainPanel);
 		
 		table = new JTable();
+		buildJTable();
+		mainPanel.setViewportView(table);
+		
+		Component horizontalStrut_1 = Box.createHorizontalStrut(215);
+		horizontalBox.add(horizontalStrut_1);
+		
+		Component verticalStrut_1 = Box.createVerticalStrut(20);
+		verticalBox.add(verticalStrut_1);
+		
+		Box horizontalBox_1 = Box.createHorizontalBox();
+		verticalBox.add(horizontalBox_1);
+		
+		JButton btnDone = new JButton("Done");
+		btnDone.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				Vector dataVector = ((DefaultTableModel)table.getModel()).getDataVector();
+				
+				System.out.println("call service");
+			}
+		});
+		btnDone.setFont(new Font("Tahoma", Font.BOLD, 11));
+		horizontalBox_1.add(btnDone);
+		
+		Component horizontalStrut_2 = Box.createHorizontalStrut(20);
+		horizontalBox_1.add(horizontalStrut_2);
+		
+		JButton btnClear = new JButton("Clear");
+		btnClear.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				buildJTable();
+			}
+		});
+		btnClear.setFont(new Font("Tahoma", Font.BOLD, 10));
+		horizontalBox_1.add(btnClear);
+
+
+
+		JPanel bottomPanel = new JPanel();
+		contentPane.add(bottomPanel, BorderLayout.SOUTH);
+
+	}
+
+	private void buildJTable() {
 		table.setFont(new Font("Tahoma", Font.PLAIN, 24));
 		table.setRowHeight(40);
 		// table.setPreferredScrollableViewportSize(new Dimension(20, 20));
-		table.setModel(new DefaultTableModel(
-			new Object[][] {
-				{"1", null, null, null},
-				{"...", null, "", null},
-			},
-			new String[] {
-				"#", "Apuesta", "Numero", "Posicion"
-			}
-		) {
-			Class[] columnTypes = new Class[] {
-				String.class, Float.class, String.class, Integer.class
-			};
-			public Class getColumnClass(int columnIndex) {
-				return columnTypes[columnIndex];
-			}
-			boolean[] columnEditables = new boolean[] {
-				false, true, true, true
-			};
-			public boolean isCellEditable(int row, int column) {
-				return columnEditables[column] && row!=(this.getRowCount()-1);
-			}
-		});
+		table.setModel(createModel());
 		
 		table.getColumnModel().getColumn(0).setPreferredWidth(40);
 		table.getColumnModel().getColumn(0).setMinWidth(5);
@@ -132,8 +204,9 @@ public class TicketPage extends JFrame {
 		table.getColumnModel().getColumn(3).setPreferredWidth(50);
 		table.getColumnModel().getColumn(3).setMinWidth(50);
 		table.getColumnModel().getColumn(3).setMaxWidth(100);
-		DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
-		centerRenderer.setHorizontalAlignment( JLabel.CENTER );
+		
+//		DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
+//		centerRenderer.setHorizontalAlignment( JLabel.CENTER );
 		
 		table.getColumnModel().getColumn(2)
 				.setCellRenderer(new DefaultTableCellRenderer() {
@@ -175,32 +248,31 @@ public class TicketPage extends JFrame {
 		});
 		
 //		table.updateUI();
-		mainPanel.setViewportView(table);
-		
-		Component horizontalStrut_1 = Box.createHorizontalStrut(40);
-		horizontalBox.add(horizontalStrut_1);
-		
-		Component verticalStrut_1 = Box.createVerticalStrut(20);
-		verticalBox.add(verticalStrut_1);
-		
-		Box horizontalBox_1 = Box.createHorizontalBox();
-		verticalBox.add(horizontalBox_1);
-		
-		JButton btnDone = new JButton("Done");
-		btnDone.setFont(new Font("Tahoma", Font.BOLD, 11));
-		horizontalBox_1.add(btnDone);
-		
-		Component horizontalStrut_2 = Box.createHorizontalStrut(20);
-		horizontalBox_1.add(horizontalStrut_2);
-		
-		JButton btnClear = new JButton("Clear");
-		btnClear.setFont(new Font("Tahoma", Font.BOLD, 10));
-		horizontalBox_1.add(btnClear);
+	}
 
-
-
-		JPanel bottomPanel = new JPanel();
-		contentPane.add(bottomPanel, BorderLayout.SOUTH);
-
+	@SuppressWarnings({ "unchecked", "rawtypes", "serial" })
+	private DefaultTableModel createModel() {
+		return new DefaultTableModel(
+			new Object[][] {
+				{"1", null, null, null},
+				{"...", null, "", null},
+			},
+			new String[] {
+				"#", "Apuesta", "Numero", "Posicion"
+			}
+		) {
+			Class[] columnTypes = new Class[] {
+				String.class, Float.class, String.class, Integer.class
+			};
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+			boolean[] columnEditables = new boolean[] {
+				false, true, true, true
+			};
+			public boolean isCellEditable(int row, int column) {
+				return columnEditables[column] && row!=(this.getRowCount()-1);
+			}
+		};
 	}
 }
